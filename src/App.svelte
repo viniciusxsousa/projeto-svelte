db908ef828060c38b6502d5b02ca08ebbdcedafe
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-06-05 20:53:14 -0300

    limpando a estrutura padrão do svelte e iniciando o projeto

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
-	let name: string = 'Vinicius';
+	import Titulo from "./components/Titulo.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="app">
+	<header>
+		<Titulo/>
+	</header>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.app {
+		max-height: 100vh;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
