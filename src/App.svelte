de2deda0b66f814da75801ef8be736182220aa1a
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-06-05 21:20:22 -0300

    criando o formulário de busca

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,16 @@
 <div class="app">
 	<header>
 		<Titulo/>
+
+		<div class="busca-usuario">
+			<form>
+				<input type="text" class="input">
+
+				<div class="botao-container">
+					<button type="submit" class="botao" >Buscar</button>
+				</div>
+			</form>
+		</div>
 	</header>
 </div>
 
@@ -18,5 +28,59 @@
 		align-items: center;
 		justify-content: space-between;
 	}
+
+	.busca-usuario {
+    position: relative;
+    width: 70%;
+  }
+
+  .input {
+    padding: 15px 25px;
+    width: calc(100% - 8.75rem);
+    font-size: 1rem;
+    border-radius: 8px;
+    border: 1px solid #2e80fa;
+    box-shadow: 0px 17px 52px rgba(222, 231, 247, 0.4);
+    outline: 0;
+  }
+
+  .input::placeholder {
+    font-family: "Roboto";
+    font-style: italic;
+    font-weight: 300;
+    font-size: 19.5px;
+    line-height: 26px;
+    color: #6e8cba;
+  }
+
+  .botao-container {
+    position: absolute;
+    width: 9.625rem;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    display: flex;
+  }
+
+  .botao {
+    padding: 15px 24px;
+    border-radius: 8px;
+    border: none;
+    background: #2e80fa;
+    line-height: 26px;
+    color: #fff;
+    font-size: 22px;
+    cursor: pointer;
+
+    transition: background-color 0.2s;
+
+    display: flex;
+    align-items: center;
+    gap: 13px;
+  }
+
+  .botao:hover {
+    background: #4590ff;
+  }
 
 </style>
